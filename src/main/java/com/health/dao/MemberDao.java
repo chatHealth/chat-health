ad38567acf7dc02440126c269e089f04a4784b1e
Bae Chanyong <nabi3315@gmail.com>
2023-10-20 17:44:08 +0900

    미완성. 일단 커밋.집가서 하자

---
+++ src/main/java/com/health/dao/MemberDao.java
@@ -0,0 +1,38 @@
+package com.health.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.jjang051.dto.MemberDto;
+import com.jjang051.mybatis.MybatisConnectionFactory;
+
+public class MemberDao {
+	public MemberDto loginMember(MemberDto memberDto) {
+		MemberDto loggedMember = null;
+		SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+		loggedMember = sqlSession.selectOne("loginMember",memberDto);
+		sqlSession.close();
+		return loggedMember;
+	}
+	public int idCheck(String id) {
+		int result = 0;
+		SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+		result = sqlSession.selectOne("idCheck",id);
+		sqlSession.close();
+		return result;
+	}
+	public int insertMember(MemberDto memberDto) {
+		int result = 0;
+		SqlSession sqlSession = MybatisConnectionFactory.getSqlSession();
+		result = sqlSession.insert("insertMember",memberDto);
+		sqlSession.close();
+		return result;
+	}
+}
+
+
+
+
+
+
+
+
