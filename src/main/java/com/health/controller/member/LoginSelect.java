e487af55be5e666cd902f27cbd5967dd3d9f10f2
Bae Chanyong <nabi3315@gmail.com>
2023-10-26 11:38:05 +0900

    login select start

---
+++ src/main/java/com/health/controller/member/LoginSelect.java
@@ -0,0 +1,30 @@
+package com.health.controller.member;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login-select")
+public class LoginSelect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginSelect() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("/WEB-INF/member/loginSelect.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
