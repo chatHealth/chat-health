b332c20726fe877886d9ecdacc434df5d4376526
Bae Chanyong <nabi3315@gmail.com>
2023-10-26 11:16:46 +0900

    라인변경

--- src/main/java/com/health/controller/member/MemberLoginProcess.java
+++ src/main/java/com/health/controller/member/MemberLoginProcess.java
@@ -31,7 +31,7 @@ public class MemberLoginProcess extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userID= request.getParameter("userID");
-		String userPW= request.getParameter("userPW");
+		String userPW= request.getParameter("userPW");		
 		String isEnterprise = request.getParameter("isEnterprise");
 
 		
