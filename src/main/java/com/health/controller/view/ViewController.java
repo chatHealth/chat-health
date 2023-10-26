97ee70147ad9c840bd2313e1afc20c038a40683b
tkfkd <tkfkd5988@gmail.com>
2023-10-26 19:21:38 +0900

    three merge- rang yong sun [10.26]

--- src/main/java/com/health/controller/view/ViewController.java
+++ src/main/java/com/health/controller/view/ViewController.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/view/Product")
+@WebServlet("/view/product")
 public class ViewController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
