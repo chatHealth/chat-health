97ca387faa096d0a96bc0d21e752c2528465ffdd
gyuusun <moni1227a@gmail.com>
2023-10-26 17:30:42 +0900

    review [10.26 sun]

---
+++ src/main/java/com/health/dao/ReviewDao.java
@@ -0,0 +1,19 @@
+package com.health.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.health.dto.ReviewDto;
+import com.health.mybatis.MyBatisConnectionFactory;
+
+public class ReviewDao {
+	public int insertRivew(ReviewDto reviewDto) {
+		int result = 0;
+		SqlSession sqlSession = MyBatisConnectionFactory.getSqlSession();
+		result = sqlSession.insert("insertRivew",reviewDto);
+		sqlSession.close();
+		return result;
+	}
+}
+
+
+
