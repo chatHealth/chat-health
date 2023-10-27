c3f3047cf1546d9295a51387b57beac503cb62af
gyuusun <moni1227a@gmail.com>
2023-10-27 16:44:27 +0900

    table전송 및css

--- src/main/java/com/health/controller/review/ReviewAdd.java
+++ src/main/java/com/health/controller/review/ReviewAdd.java
@@ -5,33 +5,67 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-/**
- * Servlet implementation class ReviewAdd
- */
+import org.apache.catalina.Session;
+
+import com.health.dao.ReviewDao;
+import com.health.dto.MemberDto;
+import com.health.dto.ReviewDto;
+import com.health.util.ModalState;
+
+
+
+@WebServlet("/review/ReviewAdd")
 public class ReviewAdd extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ReviewAdd() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ReviewDto reviewDto = new ReviewDto();
+		ReviewDao reviewDao = new ReviewDao();
+		
+//		HttpSession loggedSession = request.getSession();
+//		MemberDto loggedMember = (MemberDto)loggedSession.getAttribute("loggedMember");
+		
+		//System.out.println(loggedMember.getUserNo());
+		
+		String recontent = request.getParameter("recontent");
+		String retitle = request.getParameter("retitle");
+		
+		
+		
+		//reviewDto.setUserNo(loggedMember.getUserNo());
+		
+		reviewDto.setUserNo(1231);
+		reviewDto.setPostNo(5);		
+		reviewDto.setTitle(retitle);
+		reviewDto.setContent(recontent);
+		reviewDto.setHelpful(0);
+		
+		int result = reviewDao.insertRivew(reviewDto);
+		
+		
+		
+		
+		if(result>0) {
+			//String msg = URLEncoder.encode("글이 등록되었습니다.", StandardCharsets.UTF_8);
+			//response.sendRedirect("../board/list?state=show&msg="+msg);
+			
+			HttpSession session = request.getSession();
+			response.sendRedirect("../view/product");
+			
+//			ModalState modalState = new ModalState("show","글이 등록되었습니다.");
+//			session.setAttribute("modalState", modalState);
+//			response.sendRedirect("../board/list");
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
---
+++ src/main/webapp/WEB-INF/view/viewInclude/viewProduct.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+	
+	
+	<div class="productbox">
+			<div class="box">
+				<div class="imgbox">
+					<img src="productEx.jpg" alt="" class="imgtest">
+				</div>
+			</div>
+			<div class="box2">
+				<div id="titile">제품이름</div>
+				<div id="Name">회사이름</div>
+				<div id="RegDate">등록날자</div>
+				<div id="MaterialNo">주성분</div>
+			</div>
+		</div>
+	
+	
+</body>
+</html>
