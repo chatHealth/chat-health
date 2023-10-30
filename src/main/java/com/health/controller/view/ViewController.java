275aec277f545bfb1e2408153dc9f48bfb266929
gyuusun <moni1227a@gmail.com>
2023-10-30 16:30:36 +0900

    product1.jsp

--- src/main/java/com/health/controller/view/ViewController.java
+++ src/main/java/com/health/controller/view/ViewController.java
@@ -34,7 +34,7 @@ public class ViewController extends HttpServlet {
 		session.getAttribute("loggedMember");
 		
 		RequestDispatcher dispatcher = 
-				request.getRequestDispatcher("/WEB-INF/view/Product.jsp");
+				request.getRequestDispatcher("/WEB-INF/view/product.jsp");
 		dispatcher.forward(request, response);
 	}
 
