d230dacd3220ed562d93e5b89e002c9d2ae26924
WowJo <dst04157@gmail.com>
2023-11-02 03:17:19 +0900

    feat: my review 상품명, 등록일, 따봉 오름, 내림차순 정렬

--- src/main/java/com/health/controller/personal/MemberReview.java
+++ src/main/java/com/health/controller/personal/MemberReview.java
@@ -37,9 +37,35 @@ public class MemberReview extends HttpServlet {
             }
             map.put("idx", idx);
 
+//            정렬 기준
+//            1, "TITLE";
+//            2, "REGDATE";
+//            3, "LIKES";
+
+            int by;
+            if (request.getParameter("by") == null) {
+                by = 2;
+            } else {
+                by = Integer.parseInt(request.getParameter("by"));
+            }
+            map.put("by", by);
+
+//            오름/내림차순
+//            1, "DESC"
+//            2, "ASC"
+            int sort;
+            if (request.getParameter("sort") == null) {
+                sort = 1;
+            } else {
+                sort = Integer.parseInt(request.getParameter("sort"));
+            }
+            map.put("sort", sort);
+
             int count = personalDao.memReviewsCount(userNo);
             int pages = (int) Math.ceil(count / 10.0);
 
+            request.setAttribute("by", by);
+            request.setAttribute("sort", sort);
             request.setAttribute("pages", pages);
             List<MemberReviewPageDto> memReviews = personalDao.memReview(map);
             request.setAttribute("reviews", memReviews);
