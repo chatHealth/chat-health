8643ef259e9ba1edbbf26f42e02367765484e49b
Bae Chanyong <nabi3315@gmail.com>
2023-11-01 17:44:26 +0900

    login process 코드 복잡해서 깔끔하게 정리

--- src/main/java/com/health/controller/member/MemberLoginProcess.java
+++ src/main/java/com/health/controller/member/MemberLoginProcess.java
@@ -32,77 +32,65 @@ public class MemberLoginProcess extends HttpServlet {
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userID= request.getParameter("userID");
-		String userPW= request.getParameter("userPW");
-		String isEnterprise = request.getParameter("isEnterprise");
-		String saveID = request.getParameter("saveID");
-		
-		if(isEnterprise.equals("member")) { // 1. 개인인 경우
-			MemberDto memberDto = new MemberDto();
-			MemberDto loggedMember = null;
-			memberDto.setId(userID);
-			memberDto.setPw(userPW);
-			
-			loggedMember = memberDao.loginMember(memberDto);
-			
-			if(loggedMember!=null) {
-				HttpSession loginSession = request.getSession();
-				loggedMember.setPw(null);			
-				
-				
-				if(loggedMember.getGrade() == 1) {  // 관리자
-					loginSession.setAttribute("loggedAdmin", loggedMember);
-					response.sendRedirect("../personal/member-manage");				
-				}else{ //개인
-					loginSession.setAttribute("loggedMember", loggedMember);
-					if (saveID != null) {
-						if (saveID.equals("rememberMe")) {
-							CookieManager.createCookie(response, "saveIDCookie", userID, 60 * 60 * 24 * 5);
-						} else {
-							CookieManager.deleteCookie(response, "saveIDCookie");
-						}
-					} else {
-						CookieManager.deleteCookie(response, "saveIDCookie");
-					}
-					response.sendRedirect("../index/index");
-					} 
-	
-				
-			} else {
-				ScriptWriter.alertAndBack(response, "아이디 혹은 비밀번호가 일치하지 않습니다.");
-			}
-		} else if (isEnterprise.equals("enterprise")) {	 //2. 사업자인경우
-			EnterpriseDto enterpriseDto = new EnterpriseDto();
-			EnterpriseDto loggedMember = null;
-			enterpriseDto.setId(userID);
-			enterpriseDto.setPw(userPW);
-			loggedMember = memberDao.loginEnterprise(enterpriseDto);
-			
-			if(loggedMember!=null) {  //login success
-				HttpSession loginSession = request.getSession();
-				loggedMember.setPw(null);
-				loginSession.setAttribute("loggedEnterprise", loggedMember);
-				
-				if (saveID != null) {
-					if (saveID.equals("rememberMe")) {
-						CookieManager.createCookie(response, "saveIDCookie", userID, 60 * 60 * 24 * 5);
-					} else {
-						CookieManager.deleteCookie(response, "saveIDCookie");
-					}
-				} else {
-					CookieManager.deleteCookie(response, "saveIDCookie");
-				}
-				
-				response.sendRedirect("../index/index");
-			} else {  // login fail
-				ScriptWriter.alertAndBack(response, "아이디 혹은 비밀번호가 일치하지 않습니다.");
-			}
-		}
-			
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession loginSession = request.getSession();
+        String userID = request.getParameter("userID");
+        String userPW = request.getParameter("userPW");
+        String isEnterprise = request.getParameter("isEnterprise");
+        String saveID = request.getParameter("saveID");
 
-		
+        if (isEnterprise.equals("enterprise")) { // 사업자
+            EnterpriseDto enterpriseDto = new EnterpriseDto();
+            EnterpriseDto loggedEnterprise = null;
+            enterpriseDto.setId(userID);
+            enterpriseDto.setPw(userPW);
+            loggedEnterprise = memberDao.loginEnterprise(enterpriseDto);
 
-	}
+            if (loggedEnterprise != null) { // 로그인 성공
+                loginSession = request.getSession();
+                loggedEnterprise.setPw(null);
+                loginSession.setAttribute("loggedEnterprise", loggedEnterprise);
 
+                if (saveID != null) { // 로그인 성공은 했는데 아이디 기억하기 체크했으면
+                    CookieManager.createCookie(response, "saveIDCookie", userID, 60 * 60 * 24 * 5); // 쿠키 유효기간 5일
+                } else { // 로그인 성공은 했는데 아이디 기억하기 체크 안했으면
+                    CookieManager.deleteCookie(response, "saveIDCookie");
+                }
+                response.sendRedirect("../index/index");
+            } else { // 로그인 실패
+                ScriptWriter.alertAndBack(response, "아이디 혹은 비밀번호가 일치하지 않습니다.");
+            }
+            
+        } else { // 사업자가 아닌 경우
+            MemberDto memberDto = new MemberDto();
+            MemberDto loggedMember = null;
+            memberDto.setId(userID);
+            memberDto.setPw(userPW);
+
+            loggedMember = memberDao.loginMember(memberDto);
+
+            if (loggedMember != null) { // 로그인 성공
+                loginSession = request.getSession();
+                loggedMember.setPw(null);
+
+                if (loggedMember.getGrade() == 1) { // 로그인 성공 관리자
+                    loginSession.setAttribute("loggedAdmin", loggedMember);
+                    response.sendRedirect("../personal/member-manage"); // 관리자페이지로 이동
+                } else { // 로그인 성공 일반회원
+                    loginSession.setAttribute("loggedMember", loggedMember);
+
+                    if (saveID != null) { // 로그인 성공은 했는데 아이디 기억하기 체크했으면
+                        CookieManager.createCookie(response, "saveIDCookie", userID, 60 * 60 * 24 * 5); // 쿠키 유효기간 5일
+                    } else { // 로그인 성공은 했는데 아이디 기억하기 체크 안했으면
+                        CookieManager.deleteCookie(response, "saveIDCookie");
+                    }
+                    response.sendRedirect("../index/index");
+                }
+            } else {
+                ScriptWriter.alertAndBack(response, "아이디 혹은 비밀번호가 일치하지 않습니다.");
+            }
+        }
+
+    }
 }
